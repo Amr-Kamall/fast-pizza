f73f40fc49eefec3c763e662713bdd8f7422887b
amr <amr442028@gmail.com>
2024-02-20 22:43:28 +0200

    first commit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 
 function Username() {
   const userName = useSelector((state) => state.user.username);
+  if (!userName) return;
   return (
     <div className="hidden text-sm font-semibold md:block">{userName}</div>
   );
